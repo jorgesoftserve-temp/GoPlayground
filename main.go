fa849afcbb6b2f6ba5c200dc5e5804a231c5d858
jorgesoftserve-temp <jorge.softserve@gmail.com>
2026-07-30 14:56:49 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,42 +1,54 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 )
 
+const maxConcurrentFetches = 5
+
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	manager := NewCanvasManager()
-
-	fmt.Println(
-		"Comandos: c x y | l x y n | r a b c d | f x y r | q",
-	)
-
-	for scanner.Scan() {
-		command, err := ParseCommand(scanner.Text())
-		if err != nil {
-			fmt.Println("Error:", err)
-			continue
-		}
-
-		res, err := command.Execute(manager)
-		if err != nil {
-			fmt.Println("Error:", err)
-			continue
-		}
-
-		if res.Quit {
-			return
-		}
-
-		if res.Canvas != nil {
-			fmt.Print(res.Canvas.String())
-		}
+	if len(os.Args) != 3 {
+		fmt.Printf(
+			"Uso: %s <url-inicial> <profundidad-máxima>\n",
+			os.Args[0],
+		)
+		os.Exit(1)
 	}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error leyendo la entrada:", err)
+	startURL := os.Args[1]
+
+	maxDepth, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Fprintln(
+			os.Stderr,
+			"Error: la profundidad debe ser un número entero",
+		)
+		os.Exit(1)
+	}
+
+	fetcher := NewHTTPFetcher(10 * time.Second)
+	crawler := NewCrawler(fetcher, maxConcurrentFetches)
+
+	report, err := crawler.Crawl(startURL, maxDepth)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("URLs encontradas:")
+
+	for _, crawledURL := range report.URLs {
+		fmt.Println(crawledURL)
+	}
+
+	if len(report.Errors) > 0 {
+		fmt.Println("\nErrores encontrados:")
+
+		for _, crawlError := range report.Errors {
+			fmt.Fprintln(os.Stderr, "-", crawlError)
+		}
 	}
 }
