6a8ced49f014b35ab24a409f06a406e80d92cc1a
jorgesoftserve-temp <jorge.softserve@gmail.com>
2026-08-19 10:15:01 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,42 +1,22 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"log"
+	"net/http"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	manager := NewCanvasManager()
+	http.HandleFunc("/pets", petsHandler)
+	http.HandleFunc("/appointments", appointmentsHandler)
+	http.HandleFunc("/appointments/session", sessionHandler)
 
-	fmt.Println(
-		"Comandos: c x y | l x y n | r a b c d | f x y r | q",
+	fmt.Println("Servidor corriendo en http://localhost:8080")
+
+	log.Fatal(
+		http.ListenAndServe(
+			":8080",
+			nil,
+		),
 	)
-
-	for scanner.Scan() {
-		command, err := ParseCommand(scanner.Text())
-		if err != nil {
-			fmt.Println("Error:", err)
-			continue
-		}
-
-		res, err := command.Execute(manager)
-		if err != nil {
-			fmt.Println("Error:", err)
-			continue
-		}
-
-		if res.Quit {
-			return
-		}
-
-		if res.Canvas != nil {
-			fmt.Print(res.Canvas.String())
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error leyendo la entrada:", err)
-	}
 }
